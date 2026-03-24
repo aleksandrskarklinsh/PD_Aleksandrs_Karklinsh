4af1968df075d6cd008f8fc759c5ebfba38fc56c
aleksandrskarklinsh <aleksandrs.karklinsh@gmail.com>
2026-03-24 12:35:18 +0200

    Izveidoju ievade() funkciju saturu

--- programm.py
+++ programm.py
@@ -1,3 +1,8 @@
-ievade()
-Aprekini()
+ievade():
+    kos = input("Kostīmu skaits kuru jāizgatavo: ")
+    pog = input("pogas cena: ")
+    rav = input("rāvējslēdzeja cena: ")
+    aud = input("auduma cena(par metru): ")
+    Aprekini(kos,pog,rav,aud)
+Aprekini():
 izvade()
