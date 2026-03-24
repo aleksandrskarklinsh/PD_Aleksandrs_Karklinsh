1033447cf71e4bb9ad92417241f2f8eeff9a2030
aleksandrskarklinsh <aleksandrs.karklinsh@gmail.com>
2026-03-24 12:50:09 +0200

    pabeidzu programmu

--- programm.py
+++ programm.py
@@ -1,8 +1,25 @@
-ievade():
-    kos = input("Kostīmu skaits kuru jāizgatavo: ")
-    pog = input("pogas cena: ")
-    rav = input("rāvējslēdzeja cena: ")
-    aud = input("auduma cena(par metru): ")
-    Aprekini(kos,pog,rav,aud)
-Aprekini():
-izvade()
+def ievade():
+    kos = int(input("Kostīmu skaits kuru jāizgatavo: "))
+    pog = float(input("pogas cena: "))
+    rav = float(input("rāvējslēdzeja cena: "))
+    aud = float(input("auduma cena(par metru): "))
+    Aprekini(kos, pog, rav, aud)
+
+
+def Aprekini(kos, pog, rav, aud):
+    NepiecAud = kos * 2.5
+    NepiecPog = kos*3
+    NepiecRav = kos
+    KopejIzmaksas = NepiecAud * aud + NepiecPog * pog + NepiecRav * rav
+    FaktAud = NepiecAud * 0.88  # 12 procenti ir zaudeti
+    izvade(NepiecAud, NepiecPog, NepiecRav, KopejIzmaksas, FaktAud)
+
+
+def izvade(NepiecAud, NepiecPog, NepiecRav, KopejIzmaksas, FaktAud):
+    print(
+        f"Nepieciešams: {NepiecAud} metri auduma,{NepiecPog} pogas,{NepiecRav} rāvējslēdzēju")
+    print(f"Kopejas izmaksas: {KopejIzmaksas} eiro")
+    print(f"Faktiski izmantoto auduma daudzums pec zudumiem: {FaktAud} metri")
+
+
+ievade()
